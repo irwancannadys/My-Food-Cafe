e0be37f205f4fd36311861566a3684c065af5ca7
Irwan Cannady <irwancannadys@gmail.com>
2025-12-19 19:12:25 +0700

    Initial Commit

---
+++ MyFoodCafe/MyFoodCafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFoodCafeApp.swift
+//  MyFoodCafe
+//
+//  Created by irwan on 19/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MyFoodCafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyFoodCafeTests/MyFoodCafeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyFoodCafeTests.swift
+//  MyFoodCafeTests
+//
+//  Created by irwan on 19/12/25.
+//
+
+import Testing
+@testable import MyFoodCafe
+
+struct MyFoodCafeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
